f59910ac988d812f2d50e0d5803ef46a89954920
Hilgard <hilgardvr@gmail.com>
2022-09-04 10:13:23 +0200

    Populate team driver selection

--- lib/ff1_web/templates/live/overview_live.ex
+++ lib/ff1_web/templates/live/overview_live.ex
@@ -6,8 +6,15 @@ defmodule Ff1Web.OverviewLive do
     {:ok, socket}
   end
 
+  def handle_event("driver-selected", params, socket) do
+    IO.puts("hilla")
+    IO.inspect(params)
+    #socket = assign(socket, :drivers_selected, params)
+    {:noreply, socket}
+  end
 
   def render(assigns) do
+    #IO.inspect(assigns.socket)
     if Map.has_key?(assigns, :player_id) do
       player = Player.get_player_by_id(assigns.player_id)
       ~H"""
@@ -17,12 +24,17 @@ defmodule Ff1Web.OverviewLive do
       all_drivers = Ff1.Drivers.Driver.get_all_drivers()
       ~H"""
         <h1>Pick team</h1>
-        <label for="driver1">Driver 1:</label>
-        <select name="driver1" id="driver1">
-          <%= for driver <- all_drivers do %>
-            <option value={driver.id}><%= driver.driver_name %></option>
+        <form phx-change="driver-selected" phx-submit="save">
+          <%= for i <- 1..4 do %>
+            <label>Driver <%= i %>:</label>
+            <select name={"driver#{i}"} id={"driver#{i}"}>
+                <option disabled selected value>-- select a driver --</option>
+              <%= for driver <- all_drivers do %>
+                <option value={driver.id}><%= driver.driver_name %>   <%= driver.driver_price %></option>
+              <% end %>
+            </select>
           <% end %>
-        </select>
+        </form>
       """
     end
   end
